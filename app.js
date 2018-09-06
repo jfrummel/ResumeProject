d26fc5d80ad37686c04a4056097f1d62bca7e2d4
unknown <jfrummel@gmail.com>
2018-09-06 17:59:02 -0500

    Started resume

---
+++ app.js
@@ -0,0 +1,8 @@
+let fullName = "Name= Jeremy Rummel";
+let career ="Career: Paramedic/EMS";
+
+let interests = ["Aviation", "Sports", "Guitars", "Napping"];
+
+function displayPosition(name, position, description) {
+    console.log('*' + ' ' + position + ' ' + 'at' + name + '-' + '' + description);
+}
